8d3681cf689862c500538cce3f57cef16838bdd9
Unknown <l.schuler.94@gmail.com>
2018-10-05 14:28:48 +0200

    Exercises 1.1-1.3 done

--- Ex01/Exercise1.js
+++ Ex01/Exercise1.js
@@ -12,8 +12,14 @@ var gl;
 
 // we keep all local parameters for the program in a single object
 var ctx = {
-    shaderProgram: -1
+    shaderProgram: -1,
+    aVertexPositionId: -1
 };
+
+// we keep all the parameters for drawing a specific object together
+var rectangleObject = {
+    buffer: -1
+}
 
 /**
  * Startup function to be called when the body is loaded
@@ -44,6 +50,8 @@ function initGL() {
  */
 function setUpAttributesAndUniforms(){
     "use strict";
+    // finds the index of the variable in the program
+    ctx.aVertexPositionId = gl.getAttribLocation(ctx.shaderProgram, "aVertexPosition")
 }
 
 /**
@@ -51,6 +59,15 @@ function setUpAttributesAndUniforms(){
  */
 function setUpBuffers(){
     "use strict";
+    rectangleObject.buffer = gl.createBuffer();
+    var vertices = [
+        0.5,0.5,
+        -0.5, 0.5,
+        -0.5, -0.5,
+        0.5, -0.5
+    ];
+    gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.buffer);
+    gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
 }
 
 /**
@@ -60,5 +77,11 @@ function draw() {
     "use strict";
     console.log("Drawing");
     gl.clear(gl.COLOR_BUFFER_BIT);
+
     // add drawing routines here
+    gl.bindBuffer(gl.ARRAY_BUFFER, rectangleObject.buffer);
+    gl.vertexAttribPointer(ctx.aVertexPositionId, 2, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray(ctx.aVertexPositionId);
+
+    gl.drawArrays(gl.TRIANGLE_FAN,0,4);
 }
--- Ex01/FragmentShader.glsl
+++ Ex01/FragmentShader.glsl
@@ -1,5 +1,5 @@
 precision mediump float;
 
 void main() {
-    // to complete
+    gl_FragColor=vec4(1,1,1,0);
 }
